e99b30fa97d934a4e292d39099c770add2d24062
Andrey Vladikin <andrey.vladikin@nordigy.ru>
2021-09-26 18:15:13 +0300

    closes #5. ts migration. build project

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: {
     background: "./src/background.js",
-    "content-script": "./src/content-script.js",
+    "content-script": "./src/content-script.ts",
   },
   resolve: {
     alias: {
-      ExtensionServices: path.resolve(__dirname, "src/extension-services/"),
+      ExtensionServices: path.resolve(__dirname, "./src/extension-services/"),
     },
+    extensions: [".ts", ".tsx", ".js"],
   },
   devtool: "inline-source-map",
   plugins: [
@@ -20,6 +21,12 @@ module.exports = {
       ],
     }),
   ],
+  module: {
+    rules: [
+      { test: /\.ts?$/, loader: "ts-loader" },
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
